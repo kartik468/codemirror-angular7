18504259a4062a1e7516e0cd59807fd7ae4b9217
kartikr <rkartik1992@gmail.com>
2019-03-24 09:42:42 +0530

    initial commit for adding codemirror

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, ViewChildren, AfterContentInit } from '@angular/core';
+import { CodemirrorComponent } from '@ctrl/ngx-codemirror';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'editorTest';
+export class AppComponent implements OnInit, AfterViewInit, AfterContentInit {
+
+  content = `select firstname
+from Patient`;
+
+content1 = 'var a = 3;';
+
+  @ViewChildren(CodemirrorComponent) codemirrorComponent: CodemirrorComponent[];
+
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+    // console.log(this.codemirrorComponent);
+    this.codemirrorComponent.forEach(a => console.log(a.codeMirror));
+  }
+
+  ngAfterContentInit() {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CodemirrorModule } from '@ctrl/ngx-codemirror';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    CodemirrorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
